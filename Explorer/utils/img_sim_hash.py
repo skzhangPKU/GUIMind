af1af70be98d700339e69c108716e0f63549a282
AnonymousSE202X <131878087+AnonymousSE202X@users.noreply.github.com>
2023-05-08 13:52:23 +0800

    Update img_sim_hash.py

--- Explorer/utils/img_sim_hash.py
+++ Explorer/utils/img_sim_hash.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import imagehash
 from io import BytesIO
-from appium import webdriver
-from appium_helper import AppiumLauncher
 
 def img_hash_distance(img_b,img_a):
     pil_img_a = Image.open(BytesIO(img_a))
@@ -12,28 +10,3 @@ def img_hash_distance(img_b,img_a):
     similarity = image_a_hash - image_b_hash
     print('similarity is ',similarity)
     return similarity
-
-def generate_xml():
-    appium = AppiumLauncher(4723)
-    desired_caps = {'platformName': 'Android',
-                     'platformVersion': '7',
-                     'udid': 'emulator-5554',
-                     'deviceName': 'Xiaomi10',
-                     'autoGrantPermissions': False,
-                     'fullReset': False,
-                     'resetKeyboard': True,
-                     'androidInstallTimeout': 30000,
-                     'isHeadless': False,
-                     'automationName': 'uiautomator2',
-                     'adbExecTimeout': 30000,
-                     'appWaitActivity': '*',
-                     'newCommandTimeout': 200}
-
-    driver = webdriver.Remote(f'http://127.0.0.1:4723/wd/hub', desired_caps)
-    img_a = driver.get_screenshot_as_png()
-    img_b = driver.get_screenshot_as_png()
-    distance = img_hash_distance(img_b,img_a)
-    print(distance)
-
-if __name__ == '__main__':
-    generate_xml()
